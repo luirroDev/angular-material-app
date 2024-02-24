6ab5492c28461ae02cb8f7d817e56d12cbad02ae
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-02-24 18:16:52 -0500

    progress-spinner added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
@@ -21,12 +22,14 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
     MatButtonModule,
     MatSnackBarModule,
     ReactiveFormsModule,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   form: FormGroup;
+  loading = false;
 
   constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
     this.form = this.fb.group({
@@ -36,12 +39,14 @@ export class LoginComponent {
   }
 
   public login() {
-    const user = this.form.value.user;
-    const password = this.form.value.password;
+    const user: string = this.form.value.user;
+    const password: string = this.form.value.password;
 
     if (user === 'admin' && password === 'admin') {
       // redirect
+      this.fakeLoading();
     } else {
+      // Throw an error message
       this.errorHandler();
     }
   }
@@ -52,5 +57,17 @@ export class LoginComponent {
       horizontalPosition: 'center',
       verticalPosition: 'top',
     });
+
+    this.form.reset({
+      user: '',
+      password: '',
+    });
+  }
+
+  private fakeLoading() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
   }
 }
