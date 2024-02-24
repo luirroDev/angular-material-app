cb69448d8c5ebbe0f0178736b1437b8ab7abe6d2
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-02-24 18:38:56 -0500

    routing config

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [DashboardComponent],
+  imports: [CommonModule, DashboardRoutingModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
