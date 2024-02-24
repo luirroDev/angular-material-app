73ac1afcbb805907b6c4f8dc1d6bc6d5a509418d
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-02-24 17:53:07 -0500

    added validators forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+// angular-material
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  public login() {
+    const user = this.form.value.user;
+    const password = this.form.value.password;
+
+    if (user === 'admin' && password === 'admin') {
+      // redirect
+    } else {
+      this.errorHandler();
+    }
+  }
+
+  private errorHandler() {
+    this._snackBar.open('Usuario o contraseña incorrectos', undefined, {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+}
