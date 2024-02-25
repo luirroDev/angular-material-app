05caacd9d81807a754f04d48399ab776e8853a14
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-02-25 15:25:09 -0500

    added tooltip

--- src/app/components/dashboard-module/users/users.component.ts
+++ src/app/components/dashboard-module/users/users.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
+import { user } from '../../../interfaces/user.interface';
+// material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
-import { user } from '../../../interfaces/user.interface';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const ELEMENT_DATA: user[] = [
   { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
@@ -25,7 +20,13 @@ const ELEMENT_DATA: user[] = [
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [MatToolbarModule, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatToolbarModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
