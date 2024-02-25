ddf16ccb6288446a910854f891f5a92b68019f42
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-02-25 18:04:34 -0500

    added filter

--- src/app/components/dashboard-module/users/users.component.ts
+++ src/app/components/dashboard-module/users/users.component.ts
@@ -2,20 +2,12 @@ import { Component } from '@angular/core';
 import { user } from '../../../interfaces/user.interface';
 // material
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
-const ELEMENT_DATA: user[] = [
-  { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
-  { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
-  { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
-  { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
-  { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
-  { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
-  { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
-];
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-users',
@@ -26,11 +18,27 @@ const ELEMENT_DATA: user[] = [
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
 export class UsersComponent {
   displayedColumns: string[] = ['user', 'name', 'lastName', 'sex', 'actions'];
-  dataSource = ELEMENT_DATA;
+  userList: user[] = [
+    { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
+    { user: 'jperez', name: 'Alexander', lastName: 'Diaz', gender: 'male' },
+    { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
+    { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
+    { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
+    { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
+    { user: 'jperez', name: 'Juan', lastName: 'Perez', gender: 'male' },
+  ];
+  dataSource = new MatTableDataSource(this.userList);
+
+  public applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
