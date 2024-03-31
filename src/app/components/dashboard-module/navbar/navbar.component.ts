495d0264dc3a04cb51febf0165493ba9e9d836d6
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-03-31 00:57:45 -0400

    added sesion logout

--- src/app/components/dashboard-module/navbar/navbar.component.ts
+++ src/app/components/dashboard-module/navbar/navbar.component.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router, RouterLink } from '@angular/router';
 import { User } from '@/app/interfaces/user.interface';
 import { ComunicationService } from '@/app/services/comunication.service';
+import { AuthService } from '@/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -23,6 +24,7 @@ import { ComunicationService } from '@/app/services/comunication.service';
 export class NavbarComponent implements OnInit {
   public currentUser: User | null = null;
   private readonly router = inject(Router);
+  private readonly authSrv = inject(AuthService);
   private readonly communicationSrv = inject(ComunicationService);
 
   ngOnInit(): void {
@@ -31,5 +33,8 @@ export class NavbarComponent implements OnInit {
     });
   }
 
-  sesionLogout() {}
+  sesionLogout() {
+    this.authSrv.logout();
+    this.router.navigate(['/login']);
+  }
 }
